cfa6f0f00b874c25ffb2001a7acbc20459aa479f
harry obaseki <harryobas@gmail.com>
2019-09-14 18:25:18 +0200

    first commit

---
+++ lib/bloomon/bouquets.rb
@@ -0,0 +1,8 @@
+require "bloomon/bouquets/version"
+
+module Bloomon
+  module Bouquets
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/bloomon/bouquets/version.rb
@@ -0,0 +1,5 @@
+module Bloomon
+  module Bouquets
+    VERSION = "0.1.0"
+  end
+end
