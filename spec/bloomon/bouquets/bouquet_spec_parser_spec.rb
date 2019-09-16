6aafe093d692b1a25c410c50e0a042a27546beee
harry obaseki <harryobas@gmail.com>
2019-09-16 13:04:40 +0200

    parse bouquet spec string - when string length is 10

--- spec/bloomon/bouquets/bouquet_spec_parser_spec.rb
+++ spec/bloomon/bouquets/bouquet_spec_parser_spec.rb
@@ -4,8 +4,14 @@ RSpec.describe Bloomon::Bouquets::BouquetSpecParser do
   describe '#parse_spec' do
     context 'bouquet spec string length is 12' do
       it 'returns a hash of spec attributes' do
-        attrs = Bloomon::Bouquets::BouquetSpecParser.parse_spec "AL2a15b5c30"
+        attrs = Bloomon::Bouquets::BouquetSpecParser.parse_spec "AL20a15b5c30"
         expect(attrs).to be_a Hash
+      end
+    end
+    context 'bouquet spec string length 10' do
+      it 'returns a hash of spec attributes' do
+          attrs = Bloomon::Bouquets::BouquetSpecParser.parse_spec "CL20a15c45"
+          expect(attrs).to be_a Hash
       end
     end
   end
